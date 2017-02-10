fa8fd2e92014a6c94b552694e983a523b65c7fff
Joseph Kagiri <sasajose26@gmail.com>
2017-02-10 15:14:15 +0300

    Modified View Skills File

--- view_skills.py
+++ view_skills.py
@@ -2,10 +2,10 @@ import sqlite3 as lite
 import sys
 
 def view_studied_skills(user_id):
-	con = lite.connect("team")
+	con = lite.connect("team.db")
 	with con:
 		cur = con.cursor()
-		sql = "SELECT * FROM userskill WHERE userid={} AND status=1".format(user_id)
+		sql = "SELECT * FROM userskill WHERE user_id={} AND status=1".format(user_id)
 		cur.execute(sql);
 
 		rows = cur.fetchall()
@@ -14,10 +14,10 @@ def view_studied_skills(user_id):
 			print(row)
 			
 def view_unstudied_skills(user_id):
-    con = lite.connect("team")
+    con = lite.connect("team.db")
     with con:
 	    cur = con.cursor()
-	    sql = "SELECT * FROM userskill WHERE userid={} AND status=0".format(user_id)
+	    sql = "SELECT * FROM userskill WHERE user_id={} AND status=0".format(user_id)
 	    cur.execute(sql);
 
 	    rows = cur.fetchall()
