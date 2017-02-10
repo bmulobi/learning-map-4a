f322403239677a3c0b5b220e125108671d205691
Joseph Kagiri <sasajose26@gmail.com>
2017-02-10 14:57:47 +0300

    Merged View Skills with the Master Branch

---
+++ view_skills.py
@@ -0,0 +1,26 @@
+import sqlite3 as lite
+import sys
+
+def view_studied_skills(user_id):
+	con = lite.connect("team")
+	with con:
+		cur = con.cursor()
+		sql = "SELECT * FROM userskill WHERE userid={} AND status=1".format(user_id)
+		cur.execute(sql);
+
+		rows = cur.fetchall()
+
+		for row in rows:
+			print(row)
+			
+def view_unstudied_skills(user_id):
+    con = lite.connect("team")
+    with con:
+	    cur = con.cursor()
+	    sql = "SELECT * FROM userskill WHERE userid={} AND status=0".format(user_id)
+	    cur.execute(sql);
+
+	    rows = cur.fetchall()
+
+	    for row in rows:
+		    print(row)
