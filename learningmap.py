c9e8c4fea021a68c4dc592c854118534bc37c901
G.A <allanohgogo@gmail.com>
2017-02-10 15:51:27 +0300

    Modified the Main Application

--- learningmap.py
+++ learningmap.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3 as lite
 import sys
+from view_skills import *
 
 class AppRun(object):
     def display_title(self):
@@ -17,10 +18,11 @@ class AppRun(object):
         print("Press[2] to view all available skills")
         print("Press[3] to add a skill")
         print("Press[4] to update skills")
-        print("Press[5] view studied and unstudied skills")
-        print("Press[6] to view progress")
+        print("Press[5] view studied skills")
+        print("Press[6] view unstudied skills")
+        print("Press[7] to view progress")
         print("\npress 'q' to quit!")
-        selection = input("\nEnter 1,2,3 or 4 \n")
+        selection = input("\nEnter 1,2,3,4,5,6 or 7\n")
         return selection
 
 # Main app part
@@ -37,65 +39,12 @@ while selection != 'q':
         input("Press Enter to continue")
 
     elif selection == 2:
-        def see_skills():
-
-            con = lite.connect('team.db')
-
-            with con:
-                con.row_factory = lite.Row
-
-                cur = con.cursor()
-                cur.execute("SELECT * FROM skills")
-
-                rows = cur.fetchall()
-
-                for row in rows:
-                    print ("{} {}".format(row["skillid"], row["skillname"]))
-                    # print("A list of all the available skills \n")
+        print("A list of all the available skills \n")
         # app.display_last_event()
         input("Press Enter to continue")
 
     elif selection == 3:
-        def add_skill():
-            your_name = input("Type your name")
-            skill_to_add = input("Type one of this skills: php,python,javascript,css,html,c++,c,java,perl")
-
-            if skill_to_add == "php":
-                sid = 1
-            if skill_to_add == "python":
-                sid = 2
-            if skill_to_add == "javascript":
-                sid = 3
-            if skill_to_add == "css":
-                sid = 4
-            if skill_to_add == "html":
-                sid = 5
-            if skill_to_add == "c++":
-                sid = 6
-            if skill_to_add == "c":
-                sid = 7
-            if skill_to_add == "java":
-                sid = 8
-            if skill_to_add == "perl":
-                sid = 9
-
-            con = lite.connect('team.db')
-
-            with con:
-
-                con.row_factory = lite.Row
-
-                cur = con.cursor()
-                cur.execute("SELECT * FROM user WHERE name = 'your_name'")
-
-                rows = cur.fetchall()
-
-                for row in rows:
-                    id = row["userid"];
-
-                cur.execute("INSERT INTO userskill(user_id, skill_id, status) VALUES ('id','sid',0)")
-                print('Done')
-        # print("Add a skill to be learned")
+        print("Add a skill to be learned")
         # add_event()
         input("Press Enter to continue")
 
@@ -105,12 +54,22 @@ while selection != 'q':
         input("Press Enter to continue")
 
     elif selection == 5:
-        print("A list of all the studied and unstudied skills\n")
-        # app.display_last_event()
+        print("A list of all the studied skills\n")
+        id= input("Enter User ID")
+        view_studied_skills(id)
+       # app.display_last_event()
         input("Press Enter to continue")
 
     elif selection == 6:
-        print("Progress is \n")
+        print("A list of all the unstudied skills\n")
+        id = input("Enter User ID")
+        view_studied_skills(id)
+        # app.display_last_event()
+        input("Press Enter to continue")
+
+    elif selection == 7:
+        view_studied_skills(user_id=3)
+        # print("Progress is \n")
         # app.display_last_event()
         input("Press Enter to continue")
 
